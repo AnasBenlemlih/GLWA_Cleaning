c6ce4c488e8651090d8df190d718f00ef9c3ccb1
AnasBenlemlih <anas.benlemlih@gmail.com>
2020-05-27 19:03:57 +0000

    v1

--- app/src/main/java/com/dev/glwa_cleaning/LoginActivity.java
+++ app/src/main/java/com/dev/glwa_cleaning/LoginActivity.java
@@ -2,13 +2,31 @@ package com.dev.glwa_cleaning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+   private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses buttonHo
+                openHome();
+            }
+        });
+
+
     }
+    public void openHome(){
+        Intent intent = new Intent(this,HomeActivity.class);
+        startActivity(intent);
+    }
+
 }

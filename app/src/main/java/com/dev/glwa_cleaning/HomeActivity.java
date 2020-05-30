d8812632f65f00fb9f8d766f1b9d508538bc0084
AnasBenlemlih <anas.benlemlih@gmail.com>
2020-05-30 01:36:25 +0000

    GESTION DES ROLES

--- app/src/main/java/com/dev/glwa_cleaning/HomeActivity.java
+++ app/src/main/java/com/dev/glwa_cleaning/HomeActivity.java
@@ -1,21 +1,62 @@
 package com.dev.glwa_cleaning;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private LinearLayout linearLayoutParam;
-    private LinearLayout linearLayoutTache;
+    private LinearLayout linearLayoutParam  , linearLayoutTache;
+    CardView linearLayoutOuvrier;
+
+    TextView userName,userEmail;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        userName = findViewById(R.id.labelName);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        userId = firebaseAuth.getCurrentUser().getUid();
+
+
+        DocumentReference documentReference = firebaseFirestore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                userName.setText("Bonjour "+ documentSnapshot.getString("name"));
+
+                if (documentSnapshot.getString("type").equals("ouvrier")){
+                    linearLayoutOuvrier = findViewById(R.id.layoutOuvrier);
+                    linearLayoutOuvrier.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+
+
+
+
 
         linearLayoutParam = findViewById(R.id.parametre);
         linearLayoutParam.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/dev/glwa_cleaning/TacheActivity.java
+++ app/src/main/java/com/dev/glwa_cleaning/TacheActivity.java
@@ -1,17 +1,31 @@
 package com.dev.glwa_cleaning;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
 public class TacheActivity extends AppCompatActivity {
 
     private LinearLayout linearLayoutAddTache;
     private LinearLayout linearLayoutMesTache;
     private LinearLayout linearLayoutAllTache;
+
+    CardView addcard, mescard , allcard;
+    String userId;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
 
 
 
@@ -20,6 +34,44 @@ public class TacheActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tache);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        userId = firebaseAuth.getCurrentUser().getUid();
+
+        addcard =findViewById(R.id.addtachecard);
+        mescard = findViewById(R.id.mestachecard);
+        allcard = findViewById(R.id.alltachecard);
+
+
+        DocumentReference documentReference = firebaseFirestore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+                if (documentSnapshot.getString("type").equals("ouvrier")){
+
+                    allcard.setVisibility(View.INVISIBLE);
+                    addcard.setVisibility(View.INVISIBLE);
+                }
+                if (documentSnapshot.getString("type").equals("chef")){
+
+                    mescard.setVisibility(View.INVISIBLE);
+                    addcard.setVisibility(View.INVISIBLE);
+
+                }
+                if (documentSnapshot.getString("type").equals("admin")){
+
+                    mescard.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+
+
+
 
         linearLayoutAddTache = findViewById(R.id.addtache);
         linearLayoutAddTache.setOnClickListener(new View.OnClickListener() {
